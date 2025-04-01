d4f5e7e03ff8bb4e0349c8c5c7b27ae913f01a07
Elorm Ofori <Ofori.elorm@gmail.com>
2025-04-01 21:27:01 +0000

    fix to token expiry

--- KidSync/KidSync/settings.py
+++ KidSync/KidSync/settings.py
@@ -10,9 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 import os
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
-    'whitenoise.runserver_nostatic'
     'KiddieRemind', #My Custom app
 ]
 
@@ -83,7 +82,10 @@ WSGI_APPLICATION = 'KidSync.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
@@ -149,4 +151,7 @@ REST_FRAMEWORK = {
     ),
 }
 
-
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),  # Access token lasts 1 hour (change as needed)
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),  # Refresh token lasts 7 days (optional tweak)
+}
